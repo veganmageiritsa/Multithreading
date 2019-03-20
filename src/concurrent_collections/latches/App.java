7cc301b81da7b360439c3ad16d3aaa325bbedb09
nikolas <nikolas.leontiou@vodafoneinnovus.com>
2019-03-20 16:22:38 +0200

    completableFuture

--- src/concurrent_collections/latches/App.java
+++ src/concurrent_collections/latches/App.java
@@ -1,9 +1,13 @@
 package concurrent_collections.latches;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Worker implements Runnable {
@@ -38,8 +42,19 @@ public class App {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
         CountDownLatch latch=new CountDownLatch(5);
+        List<Worker> workers;
+
         IntStream.range(0,5).forEach((i)->
         executorService.execute(new Worker(i,latch)));
+
+        workers = IntStream.range(0, 5).mapToObj(i -> new Worker(i, latch)).collect(Collectors.toList());
+               workers.stream().forEach(CompletableFuture::runAsync);
+
+        IntStream.range(0, 5)
+                .mapToObj(i -> new Worker(i, latch))
+                .collect(Collectors.toList())
+                .stream()
+                .forEach(CompletableFuture::runAsync);
 
         try {
             latch.await();
